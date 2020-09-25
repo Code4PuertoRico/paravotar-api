6ac02a58cd7694af780d7536feb0ca07705e2292
Jose Padilla <jpadilla@lyft.com>
2020-09-25 13:53:56 -0700

    moca

--- papeleta-pdf-extractor/src/index.ts
+++ papeleta-pdf-extractor/src/index.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import sharp from 'sharp';
 import { getOCRText } from './ocr';
 import { USE_OCR } from './settings';
-import { config } from './config/2020/aguadilla/035-legislativa';
+import { config } from './config/2020/moca/036-legislativa';
 
 const originalImage = path.resolve(__dirname, config.filePath);
 
