b2b52d399f69a2cb16b66928f422b2b9db16e7f3
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-09-26 19:11:58 -0400

    Moar!!

--- papeleta-pdf-extractor/src/index.ts
+++ papeleta-pdf-extractor/src/index.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import sharp from 'sharp';
 import { getOCRText } from './ocr';
 import { USE_OCR } from './settings';
-import { config } from './config/2020/penuelas/municipal';
+import { config } from './config/2020/arroyo/municipal';
 
 const originalImage = path.resolve(__dirname, config.filePath);
 
