d5b681dff3c83aeb080a8afec17f467ebe91c271
Jose Padilla <jpadilla@lyft.com>
2020-09-28 18:35:42 -0700

    fix consulta

--- lambdas/voterStatus.ts
+++ lambdas/voterStatus.ts
@@ -22,153 +22,6 @@ const papeletasFile = "papeletas.json";
 
 const folder = "/papeletas/2016";
 
-const handler = async (event: any) => {
-  try {
-    const executablePath = await chromium.executablePath;
-
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath,
-    });
-
-    const voterId = _.get(event, "queryStringParameters.voterId", null);
-
-    if (!voterId) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: "invalid or missing voterId" }),
-      };
-    }
-
-    const page = await browser.newPage();
-    await page.goto(ceeUrl);
-
-    await page.evaluate((n) => {
-      (document.querySelector(
-        'input[name="txtNumElectoral"]'
-      ) as HTMLInputElement).value = n;
-    }, voterId);
-
-    await page.$eval('input[name="btnConsulta"]', (el) =>
-      (el as HTMLInputElement).click()
-    );
-
-    await page.waitFor(2000);
-
-    const rawData = await page.evaluate(() => {
-      return Array.from(document.querySelector("#info").children)
-        .reduce((acum, curr) => acum.concat(curr), [])
-        .map((t) => t.innerText)
-        .reduce(
-          (acum, curr) => ({
-            ...acum,
-            [curr.split(":")[0]]: curr.split(":")[1].trim(),
-          }),
-          {}
-        );
-    });
-
-    const data = Object.keys(rawData)
-      .map((k) => ({
-        [_.camelCase(k)]: rawData[k],
-      }))
-      .reduce((acum, curr) => {
-        return {
-          ...acum,
-          ...curr,
-        };
-      }, {});
-
-    const precintos = JSON.parse(
-      (
-        await S3.getObject({
-          Bucket: bucket,
-          Key: precintsFile,
-          ResponseContentType: "application/json",
-        }).promise()
-      ).Body.toString()
-    );
-
-    const counties = JSON.parse(
-      (
-        await S3.getObject({
-          Bucket: bucket,
-          Key: countiesFile,
-          ResponseContentType: "application/json",
-        }).promise()
-      ).Body.toString()
-    );
-
-    const papeletas = JSON.parse(
-      (
-        await S3.getObject({
-          Bucket: bucket,
-          Key: papeletasFile,
-          ResponseContentType: "application/json",
-        }).promise()
-      ).Body.toString()
-    );
-
-    const p = _.get(precintos, data.precinto, null);
-
-    let legislativo = p.papeleta.split("/");
-
-    legislativo = `${folder}/${_.lowerCase(
-      decodeURIComponent(
-        legislativo[legislativo.length - 1].split(".").slice(0, -1).join(".")
-      )
-    )
-      .split(" ")
-      .join("-")}`;
-
-    let municipal = counties[_.camelCase(p.pueblo)].split("/");
-
-    municipal = `${folder}/${_.lowerCase(
-      decodeURIComponent(
-        municipal[municipal.length - 1].split(".").slice(0, -1).join(".")
-      )
-    )
-      .split(" ")
-      .join("-")}`;
-
-    let estatal = papeletas[0].papeletaLink.split("/");
-
-    estatal = `${folder}/${_.lowerCase(
-      decodeURIComponent(
-        estatal[estatal.length - 1].split(".").slice(0, -1).join(".")
-      )
-    )
-      .split(" ")
-      .join("-")}`;
-
-    const payload = {
-      ...data,
-      pueblo: p.pueblo,
-      papeletas: {
-        legislativo,
-        municipal,
-        estatal,
-      },
-    };
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(payload, null, 2),
-    };
-  } catch (e) {
-    console.log(e);
-    return {
-      statusCode: 500,
-      body: JSON.stringify(e),
-    };
-  }
-};
-
-export const getVoterStatus = middy(handler)
-  .use(cors())
-  .use(doNotWaitForEmptyEventLoop())
-  .use(httpErrorHandler());
-
 const getConsulta = async (event: any) => {
   const voterId = _.get(event, "queryStringParameters.voterId", null);
 
@@ -183,24 +36,17 @@ const getConsulta = async (event: any) => {
     __EVENTTARGET: "",
     __EVENTARGUMENT: "",
     __VIEWSTATE:
-      "j8e9Ob/a0bGuyuMpEyF8NXnyvV13igDM7a2M5Kq0eXZZ7GF2wZHN66rB9tUtHjzluJd29d5Nx/Q8xPSonfTaxJZ4Xc72qbuiuhDF9pZcxtHC0SomzwyVFIxd/SNz/YKx",
-    __VIEWSTATEGENERATOR: "D331ABD5",
+      "V2vPTtz9H36XXWyAe/vVVKT1wVVu5DwXGFTND5rDaC4hTm2monjkiL+lD4w/Ue8SD/MPrBJgwuaLtmIIsh07O5sBtSWpiigN3MDTG8tU4s0=",
+    __VIEWSTATEGENERATOR: "669660C9",
     __EVENTVALIDATION:
-      "IcbJ6MBc2CH2sWiUW2edrQ5IKWiYYHDIPAMPZZZteP2OdYiVeH40S/UQTGXAG6mYxZol/GsBtqkeOKXVYFsY4xg7BUJjk31QN0/1HQwJUeHH7t+fRYXZKjWOFWelJO4h56mtJS4js8c80KnMY/eZomzavt+J4bWW4Q7HJagvkOwge/E/yjogoPO3xeh2yX8R/5H6A71WMzY1Y+ICAT2t3jfJWum3jOpWY0a/I7ZMDs9flYk/07f4kpNd/J00/Wo5SPEmATld062Jde3jleuKLfadz1oMXKvZG0yLcrFUnBgIs6sDhhuLgCaHCzUiJ2HtlpGnZr4dJL0jgvCJS2szA78BzGKn2xZaEOjfTxIEWo2ZXUohXJg9ACUqy+/qQQg3wQg4lcmkBVlvKC+GK7wy5g==",
+      "Xhpss61UphBcy0HQqyx9jfoy9SimyMWMbZvqsD5l67Z47PpRx1lNDHhh3OKPVM1tw7s07hyIoCFKo2qK+avvhkz64z1hwfmdM/apTSgUsrl7ZJcv1pk9HfIKovrW5qN9sHhMHHsDw+BcVWoq6n+Li4Nr8kNiKehaa2J4uYsgMZ65SmDaoWX7pJrAuE52cYhhOSI8Lix60FckIc7Ud/fG/exBTLH9wHQRvhSv1T/ThVlbHLwYAZHbztNfpxy6+/UD9mWzsVmzmWhuQgW7AsuXww==",
     txtNumElectoral: voterId,
     btnConsulta: "Buscar",
     txtNumeroElectoral: "",
     txtFechaNacimiento: "",
-    txtNombreEvento: "",
-    txtFechaEvento: "",
     txtPrecinto: "",
     txtUnidad: "",
-    txtColegio: "",
     txtEstatus: "",
-    txtPagina: "",
-    txtLinea: "",
-    txtCentro: "",
-    txtDireccion: "",
   };
 
   let resp;
@@ -208,7 +54,7 @@ const getConsulta = async (event: any) => {
   try {
     resp = await axios({
       method: "post",
-      url: "http://consulta.ceepur.org",
+      url: ceeUrl,
       data: qs.stringify(formURLFields),
       headers: {
         "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
@@ -232,14 +78,9 @@ const getConsulta = async (event: any) => {
       "0"
     ),
     unidad: root.querySelector("#txtUnidad").rawAttributes.value,
-    colegio: root.querySelector("#txtColegio").rawAttributes.value,
-    pagina: root.querySelector("#txtPagina").rawAttributes.value,
-    linea: root.querySelector("#txtLinea").rawAttributes.value,
     estatus: root.querySelector("#txtEstatus").rawAttributes.value,
     fechaDeNacimiento: root.querySelector("#txtFechaNacimiento").rawAttributes
       .value,
-    centroDeVotacion: root.querySelector("#txtCentro").rawAttributes.value,
-    direccion: root.querySelector("#txtDireccion").text,
   };
 
   if (!data.precinto) {
